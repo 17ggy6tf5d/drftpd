72b71eff8d542de3ba21d4fa372ef8835f3c2e8f
17ggy6tf5d <132782537+17ggy6tf5d@users.noreply.github.com>
2025-01-01 05:41:07 -0800

    Update NFOMissing.java

--- src/plugins/links/src/main/java/org/drftpd/links/master/types/nfomissing/NFOMissing.java
+++ src/plugins/links/src/main/java/org/drftpd/links/master/types/nfomissing/NFOMissing.java
@@ -76,7 +76,7 @@ public class NFOMissing extends LinkType {
 
     /*
      * This loops though the files, and checks to see if any end with .nfo
-     * If one does, it creates the link, if not, it deletes the link
+     * If one does, it deletes the link, if not, it creates the link
      */
     @Override
     public void doFixLink(DirectoryHandle targetDir) {
